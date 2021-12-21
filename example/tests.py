719494a17899c900d9dcd81812205c5c0cd662ec
Sabi Maharjan <smaharjan@outcodesoftware.com>
2021-12-21 09:53:13 +0545

    first commit

---
+++ example/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+
+# Create your tests here.
+class DummyTest(TestCase):
+
+    def test_dummy(self):
+        self.assertEqual(True, True)
