e689ceec15efb8f6d857a4f1596c8471024f264b
Sabi Maharjan <smaharjan@outcodesoftware.com>
2021-12-21 14:31:13 +0545

    config

--- django_cic/settings.py
+++ django_cic/settings.py
@@ -76,7 +76,14 @@ WSGI_APPLICATION = 'django_cic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        # 'NAME': '',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'root',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
     }
 }
 
